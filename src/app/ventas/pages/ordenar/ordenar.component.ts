d3a0f82aa3231f8f4f50db2addf877aed8ceab7e
maqalher <maqalher@hotmail.com>
2022-02-01 01:53:40 -0600

    add pipes

--- src/app/ventas/pages/ordenar/ordenar.component.ts
+++ src/app/ventas/pages/ordenar/ordenar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Color, Heroe } from '../../interfaces/ventas.interfaces';
 
 @Component({
   selector: 'app-ordenar',
@@ -8,9 +9,49 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OrdenarComponent implements OnInit {
 
+  mayusculas:boolean = true;
+  ordenarPor: string = '';
+
+  heroes:Heroe[] = [
+    {
+      nombre: 'Superman',
+      vuela: true,
+      color: Color.azul
+    },
+    {
+      nombre: 'Batman',
+      vuela: false,
+      color: Color.negro
+    },
+    {
+      nombre: 'Robin',
+      vuela: false,
+      color: Color.verde
+    },
+    {
+      nombre: 'Daredevil',
+      vuela: false,
+      color: Color.rojo
+    },
+    {
+      nombre: 'Linterna Verde',
+      vuela: true,
+      color: Color.verde
+    }
+  ]
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  cambiar():boolean {
+    return this.mayusculas = !this.mayusculas
+  }
+
+  cambiarOrden( valor:string ) {
+    console.log(valor);
+    this.ordenarPor = valor;
   }
 
 }
